f07d269c98b5176247dbb8190f4d39fe214bfe39
Aaron Rutkovsky <aaron.rutkovsky@gmail.com>
2013-09-09 17:41:39 -0500

    Rep url

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
-coffee -c commonjs.coffee
+out=commonjs.js
+echo "// `git ls-remote --get-url origin`" > $out
+coffee --stdio --print < commonjs.coffee >> $out
 coffee -c specs.coffee
 mocha-phantomjs reporter.html
